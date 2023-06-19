6085ac298d20a15547a41886783d324bbe0d83f9
Leonardo Santos <kuhnnhuk@outlook.com>
2023-06-19 02:14:53 -0300

    feat: task 1 done

---
+++ users/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from users.models import User
+from rest_framework.validators import UniqueValidator
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="username already taken."
+            )
+        ]
+    )
+    email = serializers.EmailField(
+        max_length=127,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="email already registered."
+            )
+        ],
+    )
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    birthdate = serializers.DateField(allow_null=True, default=None)
+    is_employee = serializers.BooleanField(read_only=True)
+    is_superuser = serializers.BooleanField(read_only=True)
+    password = serializers.CharField(max_length=122, write_only=True)
+
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_user(**validated_data)  # type: ignore
---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UserView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+urlpatterns = [
+    path("users/", UserView.as_view()),
+    path("login/", TokenObtainPairView.as_view()),
+]
